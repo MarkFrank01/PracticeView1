3e40148c7a961f8a3361591a1c5f473c3baf732a
MarkFrank01 <1123212438@qq.com>
2017-10-17 23:58:05 +0800

    ready to update

--- app/src/main/java/com/wjc/practiceview1/MainActivity.java
+++ app/src/main/java/com/wjc/practiceview1/MainActivity.java
@@ -1,13 +1,39 @@
 package com.wjc.practiceview1;
 
+import android.support.annotation.LayoutRes;
+import android.support.annotation.StringRes;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private class PageModel{
+        @LayoutRes int sampleLayoutRes;
+        @StringRes int titleRes;
+        @LayoutRes int practiceLayoutRes;
+
+        PageModel(@LayoutRes int sampleLayoutRes, @StringRes int titleRes, @LayoutRes int practiceLayoutRes) {
+            this.sampleLayoutRes = sampleLayoutRes;
+            this.titleRes = titleRes;
+            this.practiceLayoutRes = practiceLayoutRes;
+        }
     }
 }
